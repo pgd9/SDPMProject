0b32a7c2e79c78dda600b0c3fcff2456c459c296
Pratik Dhumal <pgd9@njit.edu>
2016-10-13 11:54:12 -0400

    Jump functionality

--- WorkingDirectory/UnityProject/FlipDiving/Assets/Scripts/Controller.cs
+++ WorkingDirectory/UnityProject/FlipDiving/Assets/Scripts/Controller.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.Scripts.HelperScripts;
 //using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Controller : MonoBehaviour
@@ -8,9 +9,13 @@ public class Controller : MonoBehaviour
     private Cubemap c;
     private Animator ani;
     private int count = 0;
-    private int diveAngle = 60;
+    public float diveAngle = 60;
+    public string State = Constants.State_Idle;
+    public GameObject angleDisplayObject;
+    public GameObject coin;
 
-    void Awake(){
+    void Awake()
+    {
         ani = GetComponent<Animator>();
         ani.applyRootMotion = false;
     }
@@ -19,44 +24,93 @@ public class Controller : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //c = GetComponent<Cubemap>();
         rb.isKinematic = true;
         rb.useGravity = false;
         ani = GetComponent<Animator>();
         ani.applyRootMotion = false;
+        angleDisplayObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space) && count == 0)
-        {
-            count++;
-            rb.isKinematic = false;
-            rb.useGravity = true;
-            Vector3 dir = Quaternion.AngleAxis(diveAngle, Vector3.forward) * Vector3.right;
-            //rb.AddForce(dir * 250, ForceMode.Force);
-            rb.velocity = new Vector3(5f, 5f, 0);
-            rb.AddTorque(new Vector3(0, 0, 1) * -100);
-        }
-        else if (Input.GetKeyUp(KeyCode.Space) && count == 1)
-        {
-            count++;
-            rb.AddTorque(new Vector3(0, 0, 10) * -100);
-            rb.centerOfMass = new Vector3(rb.centerOfMass.x, rb.centerOfMass.y + 0.25f);
-            ani.SetBool("Crouch", true);
 
-        }
-        else if (Input.GetKeyUp(KeyCode.Space) && count == 2)
+        if ((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && State == Constants.State_Idle)
         {
+            State = Constants.State_Ready;
+            angleDisplayObject.SetActive(true);
+            // Trigger Angle Animation 
+            // Get an angle - set diveAngle = getAngle from animation
+        }
+
+        else if ((Input.GetMouseButtonDown(1) || Input.touchCount == 0) && State == Constants.State_Ready)
+        { // Input.GetKeyUp(KeyCode.Space) && count == 0
+
+            angleDisplayObject.SetActive(false);
+            diveAngle= angleDisplayObject.transform.localRotation.eulerAngles.z;
+            State = Constants.State_Jump;
+            jump();
+        }
+        else if ((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && State == Constants.State_Jump)
+        { // Input.GetKeyUp(KeyCode.Space) && count == 1
+            State = Constants.State_Tuck;
+            tuck();
+        }
+
+        else if ((Input.GetMouseButtonUp(1) || Input.touchCount == 0) && State == Constants.State_Tuck)
+        {
+            State = Constants.State_End;
+            //End of Jump;
+            // Evaluate fair jump;
             rb.ResetCenterOfMass();
             rb.angularVelocity = Vector3.zero;
             ani.SetBool("Crouch", false);
         }
+
     }
 
     private void FixedUpdate()
     {
-        
+        if (transform.localRotation.eulerAngles.x > 179 && State == Constants.State_Jump)
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
+        //if (count == 1)
+        //{
+
+        //    rb.AddTorque(new Vector3(0, 0, 10) * -100, ForceMode.Force);
+        //    //rb.centerOfMass = new Vector3(rb.centerOfMass.x, rb.centerOfMass.y + 0.25f);
+        //    ani.SetBool("Crouch", true);
+        //    print(1);
+        //}
+    }
+
+    private void tuck()
+    {
+        count++;
+        rb.AddTorque(new Vector3(0, 0, 10) * -100, ForceMode.Force);
+        //rb.centerOfMass = new Vector3(rb.centerOfMass.x, rb.centerOfMass.y + 0.25f);
+        ani.SetBool("Crouch", true);
+    }
+
+    private void jump()
+    {
+        count++;
+        rb.isKinematic = false;
+        rb.useGravity = true;
+        Vector3 dir = Quaternion.AngleAxis(diveAngle, Vector3.forward) * Vector3.right;
+        //rb.AddForce(dir * 250, ForceMode.Force);
+        rb.velocity = new Vector3(5f, 5f, 0);
+        rb.AddTorque(new Vector3(0, 0, 1) * -100);
+    }
+
+
+
+
+    // Position to jump
+    public Vector3 getHumanPosition()
+    {
+        return Vector3.zero;
+        //return rb.position;
     }
 }
---
+++ WorkingDirectory/UnityProject/FlipDiving/Assets/Scripts/Gizmo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gizmo : MonoBehaviour {
+
+    public float gizmoSize = 1f;
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+}
