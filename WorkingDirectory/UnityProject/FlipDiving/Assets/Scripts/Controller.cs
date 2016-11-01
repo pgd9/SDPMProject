bb9338c9a9f72ad7056aad7076a2edf74b3f3f5d
Pratik Dhumal <pgd9@njit.edu>
2016-11-01 17:17:53 -0400

    JumpComplete

--- WorkingDirectory/UnityProject/FlipDiving/Assets/Scripts/Controller.cs
+++ WorkingDirectory/UnityProject/FlipDiving/Assets/Scripts/Controller.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Assets.Scripts.HelperScripts;
+using UnityEngine.UI;
 //using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Controller : MonoBehaviour
@@ -9,15 +10,21 @@ public class Controller : MonoBehaviour
     private Cubemap c;
     private Animator ani;
     private int count = 0;
+    private bool hasTuck;
+    private bool validLanding;
+
     public float diveAngle = 60;
     public string State = Constants.State_Idle;
     public GameObject angleDisplayObject;
     public GameObject coin;
+    public Text JumpMsg;
+    private Quaternion originalposition;
 
     void Awake()
     {
         ani = GetComponent<Animator>();
         ani.applyRootMotion = false;
+        originalposition = transform.rotation;
     }
 
     // Use this for initialization
@@ -28,36 +35,36 @@ public class Controller : MonoBehaviour
         rb.useGravity = false;
         ani = GetComponent<Animator>();
         ani.applyRootMotion = false;
-        angleDisplayObject.SetActive(false);
+        //angleDisplayObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if ((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && State == Constants.State_Idle)
+        if ((Input.GetKeyDown(KeyCode.Alpha1) || Input.touchCount > 0) && State == Constants.State_Idle)
         {
             State = Constants.State_Ready;
-            angleDisplayObject.SetActive(true);
+          //  angleDisplayObject.SetActive(true);
             // Trigger Angle Animation 
             // Get an angle - set diveAngle = getAngle from animation
         }
 
-        else if ((Input.GetMouseButtonDown(1) || Input.touchCount == 0) && State == Constants.State_Ready)
+        else if ((Input.GetKeyUp(KeyCode.Alpha2)) && State == Constants.State_Ready) //|| Input.touchCount == 0
         { // Input.GetKeyUp(KeyCode.Space) && count == 0
 
-            angleDisplayObject.SetActive(false);
-            diveAngle= angleDisplayObject.transform.localRotation.eulerAngles.z;
+            //angleDisplayObject.SetActive(false);
+            //diveAngle = angleDisplayObject.transform.localRotation.eulerAngles.z;
             State = Constants.State_Jump;
             jump();
         }
-        else if ((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && State == Constants.State_Jump)
+        else if ((Input.GetKeyDown(KeyCode.Alpha3) || Input.touchCount > 0) && State == Constants.State_Jump)
         { // Input.GetKeyUp(KeyCode.Space) && count == 1
             State = Constants.State_Tuck;
             tuck();
         }
 
-        else if ((Input.GetMouseButtonUp(1) || Input.touchCount == 0) && State == Constants.State_Tuck)
+        else if ((Input.GetKeyUp(KeyCode.Alpha4)) && State == Constants.State_Tuck) // || Input.touchCount == 0
         {
             State = Constants.State_End;
             //End of Jump;
@@ -73,8 +80,11 @@ public class Controller : MonoBehaviour
     {
         if (transform.localRotation.eulerAngles.x > 179 && State == Constants.State_Jump)
         {
-            rb.angularVelocity = Vector3.zero;
+            //rb.angularVelocity = Vector3.zero;
         }
+
+
+
         //if (count == 1)
         //{
 
@@ -83,6 +93,49 @@ public class Controller : MonoBehaviour
         //    ani.SetBool("Crouch", true);
         //    print(1);
         //}
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.collider.gameObject.name == "Floor")
+        {
+            print(transform.eulerAngles.x);
+            CheckValidLanding(transform.eulerAngles.x);
+            if (validLanding)
+            {
+                // Code on valid landing
+                print("Valid Jump");
+            }
+            else
+            {
+                print("Invalid jump");
+            }
+
+            print("collision");
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            transform.rotation = originalposition;
+            
+            transform.position = new Vector3(-12.5f, 4.5f, 0f);
+
+            //this.gameObject.SetActive(false);
+            //GetComponent<GameObject>().SetActive(false);// gameObject.SetActive(false);
+            //Destroy(currentObject);
+        }
+        else
+        {
+            validLanding = false;
+        }
+    }
+
+    private void CheckValidLanding(float angle)
+    {
+        // print(angle);
+        if ((140 < angle && angle < 220) || (320 < angle || angle < 40))
+        {
+            validLanding = true;
+        }
     }
 
     private void tuck()
