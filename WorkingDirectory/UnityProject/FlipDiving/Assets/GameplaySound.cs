e576974fbdc0a5f1ae9c32951b2bd82fc3aa559c
AkshayPatil26 <akshpatil26@gmail.com>
2016-12-07 15:14:26 -0500

    Functional Sound Toggle

---
+++ WorkingDirectory/UnityProject/FlipDiving/Assets/GameplaySound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplaySound : MonoBehaviour
+{
+    public bool IsMute = false;
+    // Use this for initialization
+    void Start()
+    {
+        IsMute = PlayerPrefs.GetInt("IsMute") == 1 ? true : false;
+        var audio = GetComponent<AudioSource>();
+        audio.mute = IsMute;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
